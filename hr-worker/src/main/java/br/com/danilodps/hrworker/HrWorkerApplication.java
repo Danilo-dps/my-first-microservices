610c7fb8926d6b2e7b2321d3f7481d30267b2070
Danilo Pereira da silva <danilo@marighella.danilo>
2025-02-02 13:15:51 -0300

    hr-worker implementation

--- hr-worker/src/main/java/br/com/danilodps/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/br/com/danilodps/hrworker/HrWorkerApplication.java
@@ -1,4 +1,4 @@
-package br.com.danilodps.hr_worker;
+package br.com.danilodps.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
