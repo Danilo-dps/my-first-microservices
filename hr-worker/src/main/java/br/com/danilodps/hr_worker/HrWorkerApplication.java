e604bab25ade326324dcb0955886bd5d6ce1fd36
Danilo Pereira da silva <danilo@marighella.danilo>
2025-02-02 01:45:00 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/java/br/com/danilodps/hr_worker/HrWorkerApplication.java
@@ -0,0 +1,12 @@
+package br.com.danilodps.hr_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+}
