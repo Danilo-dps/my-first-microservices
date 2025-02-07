f12a6b2bbc6844dbeea9c5e8b3b9b5ce59071c1d
Danilo Pereira da silva <danilo@marighella.danilo>
2025-02-07 14:59:03 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/main/java/br/com/danilodps/hroauth/HrOauthApplication.java
@@ -0,0 +1,12 @@
+package br.com.danilodps.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOauthApplication.class, args);
+	}
+}
